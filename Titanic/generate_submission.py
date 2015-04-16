111ecf021e7b4b599b7e5235344abaad86c52882
pinguinson <nikita.penguin@gmail.com>
2015-04-16 18:55:21 +0300

    yoba

--- Titanic/generate_submission.py
+++ Titanic/generate_submission.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 def main():
 	train = pd.read_csv('Data/train_processed.csv')
@@ -8,12 +9,12 @@ def main():
 	train_targets = train['Survived'].values
 	train = train.drop(['Survived'], axis = 1)
 	train_data = train.values
-	clf = LogisticRegression()
+	clf = RandomForestClassifier()
 	logReg = clf.fit(train_data, train_targets)
 	predicted = logReg.predict(test)
 	test['Survived'] = predicted
 	submission = pd.concat([test['PassengerId'], test['Survived']], axis=1, keys=['PassengerId', 'Survived'])
-	submission.to_csv('Data/submission.csv', index = False)
+	submission.to_csv('Data/submission_rfc.csv', index = False)
 
 if __name__=="__main__":
     main()
--- Titanic/test_model.py
+++ Titanic/test_model.py
@@ -1,20 +1,32 @@
 import pandas as pd
 import numpy as np
-from sklearn.cross_validation import train_test_split
+from sklearn.cross_validation import train_test_split, KFold
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.svm import SVC
 
-def test_model(data):
-	target = data.Survived
-	train = data.drop('Survived', axis = 1)
-	a_train, a_test, b_train, b_test = train_test_split(train, target, test_size=0.25, random_state = 42)
-	method = LogisticRegression()
+def test_method(method, a_train, a_test, b_train, b_test):
+	print method.__class__
 	method.fit(a_train, b_train)
 	print method.score(a_test, b_test)
 	print method.score(a_train, b_train)
 
+def test_methods(methods, a_train, a_test, b_train, b_test):
+	for method in methods:
+		test_method(method, a_train, a_test, b_train, b_test)
+
+def test_models(data):
+	target = data.Survived
+	train = data.drop('Survived', axis = 1)
+	a_train, a_test, b_train, b_test = train_test_split(train, target, test_size = 0.4, random_state = 42)
+
+	methods = [GaussianNB(), LogisticRegression(), RandomForestClassifier(n_estimators = 100, max_features = 4)]
+	test_methods(methods, a_train, a_test, b_train, b_test)
+
 def main():
 	data = pd.read_csv('Data/train_processed.csv')
-	test_model(data)
+	test_models(data)
 
 if __name__=="__main__":
     main()
